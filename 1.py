af9d15970cf0aa480e23877a04abf0164f83e796
Alexander <1322472@mail.ru>
2025-11-02 20:47:41 +0300

    sql

--- 1.py
+++ 1.py
@@ -1,4 +1,6 @@
 import openpyxl
+import sqlite3
+
 
 book = openpyxl.open("export.xlsx", read_only=True)
 sheet = book.active
@@ -9,10 +11,13 @@ p = []
 
 for row in range(1,10): #sheet.max_row+1
     product_name = sheet[row][0].value
-    p.append(product_name.upper())
     article = sheet[row][1].value 
     color = sheet[row][2].value
-    print(row, product_name , article, color)
+    # print(row, product_name , article, color)
+    x = [row, product_name , article, color]
+    p.append(x)
+
 
 print(p)
+
 
