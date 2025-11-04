ec6305c103d148eb458363067865bef5f4a58d1f
AlexanderAstapov <1322472@mail.ru>
2025-11-04 17:57:48 +0300

    parsing file

--- 1_read.py
+++ 1_read.py
@@ -14,13 +14,7 @@ for row in range(1,10): #sheet.max_row+1
     product_name = sheet[row][0].value
     article = sheet[row][1].value 
     color = sheet[row][2].value
-    # print(row, product_name , article, color)
-<<<<<<< HEAD
-=======
     x = [row, product_name , article, color]
     p.append(x)
->>>>>>> af9d15970cf0aa480e23877a04abf0164f83e796
 
-
-
-
+print(p)
