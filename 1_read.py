3d3dd8f4a20126733974ccced03cd7f1127a1dd5
AlexanderAstapov <1322472@mail.ru>
2025-11-04 21:49:23 +0300

    create db

--- 1_read.py
+++ 1_read.py
@@ -14,7 +14,28 @@ for row in range(1,10): #sheet.max_row+1
     product_name = sheet[row][0].value
     article = sheet[row][1].value 
     color = sheet[row][2].value
-    x = [row, product_name , article, color]
+    x = [product_name , article, color]
     p.append(x)
 
 print(p)
+
+connection = sqlite3.connect('I_seller.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS goods (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(500),
+    article VARCHAR(50),
+    color VARCHAR(50)
+    )
+    ''')
+
+connection.commit()
+
+cursor.executemany('''
+INSERT INTO goods ('name', 'article', 'color') 
+VALUES (?, ?, ?)          
+''', p)
+
+connection.commit()
